8ed6a9f11d8296b829a088108c877251ed283079
Narola Yash <yashnarola2525@gmail.com>
2026-03-05 12:18:25 +0530

    ...

--- next_labs/app/lab_28/server-action-sep-file/action.ts
+++ next_labs/app/lab_28/server-action-sep-file/action.ts
@@ -1,7 +1,7 @@
 async function server_action_sep_file(formData: FormData) {
     console.log(formData.get('fullname'));
     console.log(formData.get('email'));
-    cogiynsole.log(formData.get('password'));
+    console.log(formData.get('password'));
     console.log(formData.get('gender'));
     console.log(formData.get('age'));
     console.log(formData.get('skills'));
